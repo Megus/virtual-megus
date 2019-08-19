8ff7ced9a3bbfc65bbfbf0154354ddef69301112
Roman Petrov <megus.sugem@gmail.com>
2019-08-20 00:17:10 +0300

    More interesting pad sound

--- js/units/SynthPresets.js
+++ js/units/SynthPresets.js
@@ -24,8 +24,8 @@ const synthPresets = {
 
     'pad': {
         ampEnvelope: {attack: 0.03, decay: 4, sustain: 0, release: 0},
-        filterEnvelope: {attack: 0.03, decay: 2, sustain: 0, release: 0},
-        filter: {cutoff: 600, resonance: 2, envelopeLevel: 1000, type: 'highpass'},
+        filterEnvelope: {attack: 0.6, decay: 3, sustain: 0, release: 0},
+        filter: {cutoff: 600, resonance: 2, envelopeLevel: 1200, type: 'highpass'},
         osc: [
             {type: 'sawtooth', pitch: 0, detune: 0, level: 0.33},
             {type: 'sawtooth', pitch: 0, detune: -10, level: 0.23},
